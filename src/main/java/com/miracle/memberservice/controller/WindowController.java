7f6057bc84080e4943e6e9bb550da4d78341bdaa
kimjunyo <junn811@naver.com>
2023-11-15 00:03:26 +0900

    feat: main-page create

---
+++ src/main/java/com/miracle/memberservice/controller/WindowController.java
@@ -0,0 +1,19 @@
+package com.miracle.memberservice.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("v1")
+public class WindowController {
+    @GetMapping
+    public String index(){
+        return "index";
+    }
+
+    @GetMapping("/join")
+    public String userJoin(){
+        return "contact";
+    }
+}
