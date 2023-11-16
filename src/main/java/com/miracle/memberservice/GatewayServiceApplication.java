4764ca7cb909b7b7ac01845002386cc62388606e
kimjunyo <junn811@g.skku.edu>
2023-11-16 21:17:33 +0900

    fix: GatewayServiceApplication.java fix fix: settings.gradle fix

--- src/main/java/com/miracle/memberservice/GatewayServiceApplication.java
+++ src/main/java/com/miracle/memberservice/GatewayServiceApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class MemberServiceApplication {
+public class GatewayServiceApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(MemberServiceApplication.class, args);
+        SpringApplication.run(GatewayServiceApplication.class, args);
     }
 
 }
