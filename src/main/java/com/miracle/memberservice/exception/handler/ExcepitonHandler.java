fd34990fc370bce49c9f1b9b21c1684cbd62814f
kimjunyo <kimjunyo@github.com>
2023-11-27 00:07:28 +0900

    Fix: ExceptionHandler ClassCastException

--- src/main/java/com/miracle/memberservice/exception/handler/ExcepitonHandler.java
+++ src/main/java/com/miracle/memberservice/exception/handler/ExcepitonHandler.java
@@ -5,14 +5,12 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
-import java.net.ConnectException;
-
 @ControllerAdvice
 @Slf4j
 public class ExcepitonHandler {
-    @ExceptionHandler(value = ConnectException.class)
-    public String connectFail(ConnectException e, Model model){
-        model.addAttribute("errorMessage", e.getMessage());
+    @ExceptionHandler(value = ClassCastException.class)
+    public String connectFail(Model model){
+        model.addAttribute("errorMessage", "해당 서비스가 작동하지 않습니다");
         return "error/500";
     }
 
