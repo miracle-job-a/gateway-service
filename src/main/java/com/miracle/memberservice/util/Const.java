941a8536042468560f9abeb74b4f1e3fbd23a2ff
kimjunyo <kimjunyo@github.com>
2023-11-27 03:02:05 +0900

    Fix: Service Call Refactoring

---
+++ src/main/java/com/miracle/memberservice/util/Const.java
@@ -0,0 +1,19 @@
+package com.miracle.memberservice.util;
+
+public abstract class Const {
+
+    public static class RequestHeader {
+
+        public static final String SESSION_ID = "Session-Id";
+        public static final String MIRACLE = "Miracle";
+        public static final String ID = "id";
+        public static final String HEADER_ID = "-Id";
+        public static final String EMAIL = "email";
+        public static final String HEADER_EMAIL = "-Email";
+        public static final String BNO = "bno";
+        public static final String HEADER_BNO = "-Bno";
+        public static final String USER = "user";
+        public static final String COMPANY = "company";
+
+    }
+}
--- src/main/java/com/miracle/memberservice/util/ServiceCall.java
+++ src/main/java/com/miracle/memberservice/util/ServiceCall.java
@@ -11,128 +11,77 @@ import reactor.core.publisher.Mono;
 
 import javax.servlet.http.HttpSession;
 import java.nio.charset.StandardCharsets;
+import java.util.function.Function;
 
 public class ServiceCall {
 
-    /**
-     * get으로 보내줄 데이터와 세션 전달하여 APIResponse받아내는 메서드, url에 모든 정보가 전달됨.
-     *
-     * @param httpSession 현재 세션
-     * @param url         서비스에 어떤 요청을 할 것인지
-     * @return Webclient에 넘겨주고 받을 반응 데이터
-     */
-    public static ApiResponse get(HttpSession httpSession, String service, String url) {
+    private static final String BASE_URL = "http://localhost:";
+    private static final String VERSION = "/v1";
 
-        WebClient webClient = WebClient.builder()
-                .baseUrl("http://localhost:" + port(service))
-                .defaultHeader(HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_JSON_VALUE)
-                .build();
-
-        MiracleTokenKey key = new MiracleTokenKey(httpSession);
-
-        return webClient.get()
-                .uri(uriBuilder -> uriBuilder.path("/v1/" + url).build())
-                .header("miracle", key.getHashcode())
-                .header("sessionId", key.getSessionId())
-                .retrieve()
-                .bodyToMono(ApiResponse.class)
-                .onErrorResume(e -> {
-                    ObjectMapper om = new ObjectMapper();
-                    WebClientResponseException exception = (WebClientResponseException) e;
-                    String errorResponseBody = exception.getResponseBodyAsString(StandardCharsets.UTF_8);
-                    ApiResponse apiResponse = null;
-                    try {
-                        apiResponse = om.readValue(errorResponseBody, ApiResponse.class);
-                    } catch (JsonProcessingException ex) {
-                        ex.printStackTrace();
-                    }
-                    return Mono.just(apiResponse);
-                }).block();
+    private static WebClient.Builder createWebClientBuilder(String serviceType) {
+        return WebClient.builder()
+                .baseUrl(BASE_URL + port(serviceType))
+                .defaultHeader(HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_JSON_VALUE);
     }
 
-    /**
-     * post로 보내줄 데이터와 세션 전달하여 APIResponse받아내는 메서드
-     *
-     * @param httpSession 현재 세션
-     * @param dto         넘겨줄 값
-     * @param service     어떤 서비스에 넘겨줄지
-     * @param url         서비스에 어떤 요청을 할 것인지
-     * @return Webclient에 넘겨주고 받을 반응 데이터
-     */
-    public static ApiResponse post(HttpSession httpSession, Object dto, String service, String url) {
-
-        WebClient webClient = WebClient.builder()
-                .baseUrl("http://localhost:" + port(service))
-                .defaultHeader(HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_JSON_VALUE)
-                .build();
-
+    private static Mono<ApiResponse> addCommonHeaders(WebClient.RequestHeadersSpec<?> request, HttpSession httpSession, String serviceType) {
+        String capitalizeFirstLetter = capitalizeFirstLetter(serviceType);
         MiracleTokenKey key = new MiracleTokenKey(httpSession);
 
-        return webClient.post()
-                .uri(uriBuilder -> uriBuilder.path("/v1/" + url).build())
-                .bodyValue(dto)
-                .header("miracle", key.getHashcode())
-                .header("sessionId", key.getSessionId())
+        return request
+                .header(Const.RequestHeader.MIRACLE, key.getHashcode())
+                .header(Const.RequestHeader.SESSION_ID, key.getSessionId())
+                .header(capitalizeFirstLetter + Const.RequestHeader.HEADER_ID, String.valueOf(httpSession.getAttribute(Const.RequestHeader.ID)))
+                .header(capitalizeFirstLetter + Const.RequestHeader.HEADER_EMAIL, (String) httpSession.getAttribute(Const.RequestHeader.EMAIL))
+                .header(capitalizeFirstLetter + Const.RequestHeader.HEADER_BNO, (String) httpSession.getAttribute(Const.RequestHeader.BNO))
                 .retrieve()
                 .bodyToMono(ApiResponse.class)
-                .onErrorResume(e -> {
-                    ObjectMapper om = new ObjectMapper();
-                    WebClientResponseException exception = (WebClientResponseException) e;
-                    String errorResponseBody = exception.getResponseBodyAsString(StandardCharsets.UTF_8);
-                    ApiResponse apiResponse = null;
-                    try {
-                        apiResponse = om.readValue(errorResponseBody, ApiResponse.class);
-                    } catch (JsonProcessingException ex) {
-                        ex.printStackTrace();
-                    }
-                    return Mono.just(apiResponse);
-                }).block();
+                .onErrorResume(handleError());
     }
 
-    /**
-     * delete로 보내줄 데이터와 세션 전달하여 APIResponse받아내는 메서드
-     *
-     * @param httpSession 현재 세션
-     * @param service     어떤 서비스에 넘겨줄지
-     * @param url         서비스에 어떤 요청을 할 것인지
-     * @return Webclient에 넘겨주고 받을 반응 데이터
-     */
-    public static ApiResponse delete(HttpSession httpSession, String service, String url) {
+    public static ApiResponse get(HttpSession httpSession, String serviceType, String url) {
+        return addCommonHeaders(createWebClientBuilder(serviceType).build().get()
+                .uri(uriBuilder -> uriBuilder.path(VERSION + url).build()), httpSession, serviceType).block();
+    }
 
-        WebClient webClient = WebClient.builder()
-                .baseUrl("http://localhost:" + port(service))
-                .defaultHeader(HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_JSON_VALUE)
-                .build();
+    public static ApiResponse post(HttpSession httpSession, Object dto, String serviceType, String url) {
+        return addCommonHeaders(createWebClientBuilder(serviceType).build().post()
+                .uri(uriBuilder -> uriBuilder.path(VERSION + url).build()).bodyValue(dto), httpSession, serviceType).block();
+    }
 
-        MiracleTokenKey key = new MiracleTokenKey(httpSession);
-
-        return webClient.delete()
-                .uri(uriBuilder -> uriBuilder.path("/v1/" + url).build())
-                .header("miracle", key.getHashcode())
-                .header("sessionId", key.getSessionId())
-                .retrieve()
-                .bodyToMono(ApiResponse.class)
-                .onErrorResume(e -> {
-                    ObjectMapper om = new ObjectMapper();
-                    WebClientResponseException exception = (WebClientResponseException) e;
-                    String errorResponseBody = exception.getResponseBodyAsString(StandardCharsets.UTF_8);
-                    ApiResponse apiResponse = null;
-                    try {
-                        apiResponse = om.readValue(errorResponseBody, ApiResponse.class);
-                    } catch (JsonProcessingException ex) {
-                        ex.printStackTrace();
-                    }
-                    return Mono.just(apiResponse);
-                }).block();
+    public static ApiResponse delete(HttpSession httpSession, String serviceType, String url) {
+        return addCommonHeaders(createWebClientBuilder(serviceType).build().delete()
+                .uri(uriBuilder -> uriBuilder.path(VERSION + url).build()), httpSession, serviceType).block();
     }
 
     private static int port(String memberType) {
-        if (memberType.equals("user")) {
+        if (memberType.equals(Const.RequestHeader.USER)) {
             return 60001;
-        } else if (memberType.equals("company")) {
+        } else if (memberType.equals(Const.RequestHeader.COMPANY)) {
             return 60002;
         } else {
             return 60003;
         }
     }
+
+    private static String capitalizeFirstLetter(String input) {
+        return Character.toUpperCase(input.charAt(0)) + input.substring(1);
+    }
+
+    private static Function<Throwable, Mono<ApiResponse>> handleError() {
+        return e -> {
+            ObjectMapper om = new ObjectMapper();
+            WebClientResponseException exception = (WebClientResponseException) e;
+            String errorResponseBody = exception.getResponseBodyAsString(StandardCharsets.UTF_8);
+            ApiResponse apiResponse;
+            try {
+                apiResponse = om.readValue(errorResponseBody, ApiResponse.class);
+            } catch (JsonProcessingException ex) {
+                // 에러 응답을 만들 수 없는 경우 빈 ApiResponse를 반환
+                apiResponse = new ApiResponse();
+            }
+            return Mono.just(apiResponse);
+        };
+    }
 }
+
