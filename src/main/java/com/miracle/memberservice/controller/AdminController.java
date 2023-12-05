6293c4fc9a736edb98df6d84b688423bf91bedfe
wjdals3936 <3936vldzm@naver.com>
2023-12-05 15:22:30 +0900

    Feat: 관리자 로그아웃 API

--- src/main/java/com/miracle/memberservice/controller/AdminController.java
+++ src/main/java/com/miracle/memberservice/controller/AdminController.java
@@ -3,19 +3,27 @@ package com.miracle.memberservice.controller;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import javax.servlet.http.HttpSession;
 
 @Controller
 @RequestMapping("/v1/admin")
 @RequiredArgsConstructor
 public class AdminController {
 
-    @GetMapping("/login")
-    private String adminLogin(){ return "admin/login"; }
+    @GetMapping("/logout")
+    public String logout(HttpSession session) {
+        session.invalidate();
+        return "index";
+    }
     @GetMapping("/users")
     private String userList(){ return "admin/userList"; }
+
     @GetMapping("/stacks")
     private String stackList(){ return "admin/stackList"; }
+
     @GetMapping("/jobs")
     private String jobList(){ return "admin/jobList"; }
 
