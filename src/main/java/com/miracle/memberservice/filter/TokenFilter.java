0a8076736d4e30ffcc1e24b3c84b005f10bafd97
chocolaggibbiddori <115144342+chocolaggibbiddori@users.noreply.github.com>
2023-12-15 00:20:27 +0900

    Fix : [TokenFilter] JWT 토큰을 헤더가 아닌 쿠키에서 가져오도록 수정
    추가 수정 내역
    1. 불필요한 필드, 메서드 삭제
    2. JWT 토큰 의존성 삭제

--- src/main/java/com/miracle/memberservice/filter/TokenFilter.java
+++ src/main/java/com/miracle/memberservice/filter/TokenFilter.java
@@ -7,23 +7,22 @@ import org.springframework.util.AntPathMatcher;
 
 import javax.servlet.FilterChain;
 import javax.servlet.ServletException;
+import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpFilter;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
+import java.util.Optional;
 
 @Slf4j
 @RequiredArgsConstructor
 public class TokenFilter extends HttpFilter {
 
-    private static final String HEADER_AUTHORIZATION = "Authorization";
-
     private final List<String> excludeUriPatterns = new ArrayList<>();
     private final TokenService tokenService;
-    private HttpServletRequest httpServletRequest;
-    private HttpServletResponse httpServletResponse;
 
     public void addExcludeUriPattern(String... antPatterns) {
         excludeUriPatterns.addAll(List.of(antPatterns));
@@ -31,18 +30,19 @@ public class TokenFilter extends HttpFilter {
 
     @Override
     protected void doFilter(HttpServletRequest request, HttpServletResponse response, FilterChain chain) throws IOException, ServletException {
-        httpServletRequest = request;
-        httpServletResponse = response;
-
-        String uri = httpServletRequest.getRequestURI();
+        String uri = request.getRequestURI();
         if (isExcluded(uri)) {
             chain.doFilter(request, response);
             return;
         }
 
-        String bearerToken = getBearerToken();
-        if (bearerToken == null || !tokenService.validateToken(bearerToken)) {
-            httpServletResponse.sendError(HttpServletResponse.SC_UNAUTHORIZED);
+        Optional<String> tokenOpt = Arrays.stream(request.getCookies())
+                .filter(c -> c.getName().equals("token"))
+                .map(Cookie::getValue)
+                .findFirst();
+
+        if (tokenOpt.isEmpty() || !tokenService.validateToken(tokenOpt.get())) {
+            response.sendError(HttpServletResponse.SC_UNAUTHORIZED);
             return;
         }
 
@@ -56,15 +56,5 @@ public class TokenFilter extends HttpFilter {
         }
 
         return false;
-    }
-
-    private String getBearerToken() {
-        String bearerToken = httpServletRequest.getHeader(HEADER_AUTHORIZATION);
-
-        if (bearerToken != null && bearerToken.startsWith("Bearer ")) {
-            return bearerToken.substring(7);
-        }
-
-        return null;
     }
 }
