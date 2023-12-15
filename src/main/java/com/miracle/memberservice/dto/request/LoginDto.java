39ba47dce35caff9f04d3a738ee1a8cfb8271435
kimjunyo <kimjunyo@github.com>
2023-12-15 16:02:34 +0900

    Feat: 유저 정보 수정 시 이메일 입력 삭제

--- src/main/java/com/miracle/memberservice/dto/request/LoginDto.java
+++ src/main/java/com/miracle/memberservice/dto/request/LoginDto.java
@@ -2,6 +2,7 @@ package com.miracle.memberservice.dto.request;
 
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
+import lombok.Setter;
 import lombok.ToString;
 import org.springframework.format.annotation.DateTimeFormat;
 
@@ -9,12 +10,21 @@ import java.time.LocalDate;
 
 @ToString
 @Getter
-@RequiredArgsConstructor
 public class LoginDto {
-    private final String email;
+    @Setter
+    private String email;
     private final String password;
     private final String memberType;
     private final String postId;
     private final String companyId;
     private final String postType;
+
+    public LoginDto(String email, String password, String memberType, String postId, String companyId, String postType) {
+        this.email = email;
+        this.password = password;
+        this.memberType = memberType;
+        this.postId = postId;
+        this.companyId = companyId;
+        this.postType = postType;
+    }
 }
